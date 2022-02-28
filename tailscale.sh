5e573350250dd29c1562b74b916e9d41b7b54146
Ragnor Comerford <hello@ragnor.co>
2022-02-28 19:50:15 +0100

    Fix env variavle

--- tailscale.sh
+++ tailscale.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-until tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=flydb
+until tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=flydb
 do
     sleep 0.1
 done
